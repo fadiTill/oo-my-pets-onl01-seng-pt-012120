c9d9899741df3fd2524508782b7b3a72390a4422
fadimata tillman <fadi.tillman@gmail.com>
2020-02-20 03:33:57 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,28 @@
 class Cat
+
+  attr_accessor  :owner
+
+  def initialize (name, owner )
+    @name = name
+    @owner = owner
+
+  end
+
+  def name
+    @name
+  end
+
+# def mood
+#   @mood = mood
+# end
+
+  def mood("nervous")
+     @mood = mood("nervous")
+end
+
+def mood
+  @mood
+end
+
   # code goes here
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,43 @@
+require 'pry'
+
 class Owner
-  # code goes here
+
+  @@all = []
+
+def initialize (name,species = "human")
+   @name = name
+   @species = species
+   @@all<<self
+
+  end
+
+  def name
+    @name
+  end
+
+   def species
+   @species
+  end
+
+
+  def say_species
+    "I am a #{species}."
+  end
+
+ def self.all
+  @@all
+end
+# end
+
+def self.count
+  @@all.count
+end
+
+def self.reset_all
+  @@all.clear
+end
+# def say_species
+#   @say_species
+# end
+
 end
